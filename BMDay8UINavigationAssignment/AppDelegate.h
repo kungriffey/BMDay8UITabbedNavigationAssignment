b2712096eb1045af90dd22e95a96022f2e0fb465
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-15 15:32:27 -0400

    Initial Commit

---
+++ BMDay8UINavigationAssignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMDay8UINavigationAssignment
+//
+//  Created by Kunwardeep Gill on 2015-04-15.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMDay8UINavigationAssignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMDay8UINavigationAssignment
+//
+//  Created by Kunwardeep Gill on 2015-04-15.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
